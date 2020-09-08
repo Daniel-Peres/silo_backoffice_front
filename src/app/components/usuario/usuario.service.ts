ebf010a2a7f2320e5e5fe8d91e9f3a3450ef2184
Daniel-Peres <dsperes@gmail.com>
2020-09-08 18:39:29 -0300

    Fix-lógica da mensagem de erro e Background do input no autocomplete

--- src/app/components/usuario/usuario.service.ts
+++ src/app/components/usuario/usuario.service.ts
@@ -82,7 +82,7 @@ export class UsuarioService {
 
   showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
-      duration: 2500,
+      duration: 3000,
       horizontalPosition: "right",
       verticalPosition: "top",
       panelClass: isError ? ['msg-error'] : ['msg-sucess']
