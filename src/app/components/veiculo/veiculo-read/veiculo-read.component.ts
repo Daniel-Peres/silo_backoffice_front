b19006da063f4b8c73f413cd67fa10ef664485c7
Daniel-Peres <dsperes@gmail.com>
2020-09-17 23:31:02 -0300

    Telas CRUD Veiculo finalizadas

--- src/app/components/veiculo/veiculo-read/veiculo-read.component.ts
+++ src/app/components/veiculo/veiculo-read/veiculo-read.component.ts
@@ -23,8 +23,8 @@ export class VeiculoReadComponent implements OnInit {
   @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
     this.paginator = mp;
   }
-  usuarios = { content: [] };
-  // veiculos = { content: [] };
+  // usuarios = { content: [] };
+  veiculos = { content: [] };
   filter = '';
   displayedColumns = ['id', 'placaVeiculo', 'modeloVeiculo', 'numeroLinha', 'empresaId', 'empresa', 'totalLugares', 'lugaresSentados', 'lugaresEmPe', 'codEquipamento', 'action']
 
@@ -34,84 +34,84 @@ export class VeiculoReadComponent implements OnInit {
   public pageSizeOptions: number[] = [5, 10, 25, 100];
 
   constructor(
-    // private veiculoService: VeiculoService,
-    private usuarioService: UsuarioService,
+    private veiculoService: VeiculoService,
+    // private usuarioService: UsuarioService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.listarTodosUsuarios();
-    // this.listarTodosVeiculos();
+    // this.listarTodosUsuarios();
+    this.listarTodosVeiculos();
   }
 
   navigateToNovoVeiculo(): void {
     this.router.navigate(['veiculos/create']);
   }
 
-  listarTodosUsuarios(): void {
-    this.usuarioService.read('', this.pageSize, this.currentPage).subscribe(usuarios => {
-      this.usuarios = usuarios;
-      this.totalSize = usuarios.totalElements;
-
-      if (this.totalSize == 0)
-        this.usuarioService.showMessage2('Nenhum registro encontrado.')
-
-      if (this.dataSource == undefined) {
-        this.dataSource = new MatTableDataSource(this.usuarios.content);
-        this.dataSource.paginator = this.paginator;
-      }
-    })
-  }
-
-  // listarTodosVeiculos(): void {
-  //   this.veiculoService.read('', this.pageSize, this.currentPage).subscribe(veiculos => {
-  //     this.veiculos = veiculos;
-  //     this.totalSize = veiculos.totalElements;
+  // listarTodosUsuarios(): void {
+  //   this.usuarioService.read('', this.pageSize, this.currentPage).subscribe(usuarios => {
+  //     this.usuarios = usuarios;
+  //     this.totalSize = usuarios.totalElements;
 
   //     if (this.totalSize == 0)
   //       this.usuarioService.showMessage2('Nenhum registro encontrado.')
 
   //     if (this.dataSource == undefined) {
-  //       this.dataSource = new MatTableDataSource(this.veiculos.content);
+  //       this.dataSource = new MatTableDataSource(this.usuarios.content);
   //       this.dataSource.paginator = this.paginator;
   //     }
   //   })
   // }
 
-  getPaginatorData(event): void {
-    this.currentPage = event.pageIndex;
-    this.pageSize = event.pageSize;
-    this.listarTodosUsuarios();
-    // this.listarTodosVeiculos();
-  }
-
-  listarUsuariosFiltro(): void {
-    this.usuarioService.read(this.filter, this.pageSize, this.currentPage).subscribe(usuarios => {
-      this.usuarios = usuarios;
-      this.totalSize = usuarios.totalElements;
+  listarTodosVeiculos(): void {
+    this.veiculoService.read('', this.pageSize, this.currentPage).subscribe(veiculos => {
+      this.veiculos = veiculos;
+      this.totalSize = veiculos.totalElements;
 
       if (this.totalSize == 0)
-        this.usuarioService.showMessage2('Nenhum registro encontrado.')
+        this.veiculoService.showMessage2('Nenhum registro encontrado.')
 
       if (this.dataSource == undefined) {
-        this.dataSource = new MatTableDataSource(this.usuarios.content);
+        this.dataSource = new MatTableDataSource(this.veiculos.content);
         this.dataSource.paginator = this.paginator;
       }
     })
   }
 
-  // listarVeiculosFiltro(): void {
-  //   this.veiculoService.read(this.filter, this.pageSize, this.currentPage).subscribe(veiculos => {
-  //     this.veiculos = veiculos;
-  //     this.totalSize = veiculos.totalElements;
+  getPaginatorData(event): void {
+    this.currentPage = event.pageIndex;
+    this.pageSize = event.pageSize;
+    // this.listarTodosUsuarios();
+    this.listarTodosVeiculos();
+  }
+
+  // listarUsuariosFiltro(): void {
+  //   this.usuarioService.read(this.filter, this.pageSize, this.currentPage).subscribe(usuarios => {
+  //     this.usuarios = usuarios;
+  //     this.totalSize = usuarios.totalElements;
 
   //     if (this.totalSize == 0)
-  //       this.veiculoService.showMessage2('Nenhum registro encontrado.')
+  //       this.usuarioService.showMessage2('Nenhum registro encontrado.')
 
   //     if (this.dataSource == undefined) {
-  //       this.dataSource = new MatTableDataSource(this.veiculos.content);
+  //       this.dataSource = new MatTableDataSource(this.usuarios.content);
   //       this.dataSource.paginator = this.paginator;
   //     }
   //   })
   // }
+
+  listarVeiculosFiltro(): void {
+    this.veiculoService.read(this.filter, this.pageSize, this.currentPage).subscribe(veiculos => {
+      this.veiculos = veiculos;
+      this.totalSize = veiculos.totalElements;
+
+      if (this.totalSize == 0)
+        this.veiculoService.showMessage2('Nenhum registro encontrado.')
+
+      if (this.dataSource == undefined) {
+        this.dataSource = new MatTableDataSource(this.veiculos.content);
+        this.dataSource.paginator = this.paginator;
+      }
+    })
+  }
 }
