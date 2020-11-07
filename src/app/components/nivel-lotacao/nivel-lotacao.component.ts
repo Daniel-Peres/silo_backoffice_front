3ca609ba93914ff5882d3d20dbf7e023fb1cdfc9
Daniel-Peres <dsperes@gmail.com>
2020-11-07 00:01:01 -0300

    progresso na tela Mostrar Nível de Lotação

--- src/app/components/nivel-lotacao/nivel-lotacao.component.ts
+++ src/app/components/nivel-lotacao/nivel-lotacao.component.ts
@@ -25,7 +25,8 @@ export class NivelLotacaoComponent implements OnInit {
     id: null,
     datahora: '',
     equipamento: {
-      codEquipamento: ''
+      codEquipamento: '',
+      descricaoEquipamento: ''
     },
     veiculo: {
       placaVeiculo: '',
@@ -34,6 +35,10 @@ export class NivelLotacaoComponent implements OnInit {
     qtdPassageiros: null,
     status: ''
   };
+
+  equipamentoInstalado = '';
+  numeroDePassageiros = '';
+  statusLotacao = '';
 
   constructor(
     private veiculoService: VeiculoService,
@@ -51,13 +56,23 @@ export class NivelLotacaoComponent implements OnInit {
   }
 
   listarHistorico(): void {
-    if (this.selectedVeiculo == "") { 
+    if (this.selectedVeiculo == "") {
       this.historicoService.showMessage2("Selecione um veiculo!!!")
     } else {
       // setInterval(() => {
-        this.historicoService.readStatus(this.userEmpresaId, this.selectedVeiculo).subscribe(historico => {
+      this.historicoService.readStatus(this.userEmpresaId, this.selectedVeiculo).subscribe(historico => {
+        // Se não existir histórico para o veículo selecionado
+        if (historico == null) {
+          this.historicoService.showMessage2("Não existe histórico para esse veículo!!!");
+          this.onibusColor = "onibus.png"
+          //zerando informações do histórico que aparece na tela
+          this.zerarHistorico();
+        } else {
           this.historico = historico;
-
+          this.equipamentoInstalado = this.historico.equipamento.codEquipamento + ' - ' + this.historico.equipamento.descricaoEquipamento;
+          this.numeroDePassageiros = this.historico.qtdPassageiros + ' passageiros';
+          this.statusLotacao = '( ' + this.historico.status + ' )';
+          //mudar a cor do veículo
           if (this.historico.status == "Lotação Baixa") {
             this.onibusColor = "onibus_verde.png"
           } else if (this.historico.status == "Lotação Alta") {
@@ -65,8 +80,25 @@ export class NivelLotacaoComponent implements OnInit {
           } else {
             this.onibusColor = "onibus_amarelo.png"
           }
-        })
+        }
+      })
       // }, 10000)
     }
   }
+
+  zerarHistorico(): void {
+    this.historico.id = null;
+    this.historico.datahora = '';
+    this.historico.equipamento.codEquipamento = null;
+    this.historico.equipamento.descricaoEquipamento = '';
+    this.historico.qtdPassageiros = null;
+    this.historico.status = '';
+    this.historico.veiculo.placaVeiculo = '';
+    this.historico.veiculo.modeloVeiculo = '';
+    this.historico.veiculo.numeroLinha = '';
+    this.equipamentoInstalado = '';
+    this.numeroDePassageiros = '';
+    this.statusLotacao = '';
+  }
+
 }
